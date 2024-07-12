b1d036c4ba2d0cbf5a3e25386007857c87f8a6ae
Zahra Bukhari <bukhaz99@gmail.com>
2024-07-11 21:21:36 -0500

    video capture 6 lines

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import cv2
 cam = cv2.VideoCapture(0) #captures video, only the first one
-
+while True: #forever
+    _, frame = cam.read()
+    cv2.imshow('Eye Control Mouse', frame) #show some image called eye control mouse
+    cv2.waitKey(1) #wait for a key if i press a key, wait for 1 key
